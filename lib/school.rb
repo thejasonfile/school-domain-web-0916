23565b6fcc88f4e954839877219f99b0b5180738
Jason Arnold <jason.arnold.00@gmail.com>
2016-09-29 14:28:19 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -24,14 +24,4 @@ class School
       new_hash[grade] = students.sort 
     end
   end
-
 end
-
-school = School.new("Bayside")
-school.add_student("Homer Simpson", 9)
-school.add_student("Bart Simpson", 9)
-school.add_student("Avi Flombaum", 10)
-school.add_student("Jeff Baird", 10)
-school.add_student("Blake Johnson", 7)
-school.add_student("Jack Bauer", 7)
-binding.pry
